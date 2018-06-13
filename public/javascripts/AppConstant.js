9bfe425121877edf9ba1315ce7e9ea9fcc2d5a6a
ellisthomas <ellisthomas80@gmail.com>
2018-06-12 22:03:05 -0500

    fixed errors

---
+++ public/javascripts/AppConstant.js
@@ -0,0 +1,8 @@
+app.constant("FIREBASE_CONFIG", {
+	apiKey: "AIzaSyCOLDaQM0UKYsYNvKy8V5999SEaO71AJ5w",
+    authDomain: "portfolio-f5d45.firebaseapp.com",
+    databaseURL: "https://portfolio-f5d45.firebaseio.com",
+    projectId: "portfolio-f5d45",
+    storageBucket: "",
+    messagingSenderId: "395539347119"
+});
