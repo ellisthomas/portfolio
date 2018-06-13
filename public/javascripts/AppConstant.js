df7a5d548a3b00764d05d91d9cf7c965c6e2dac7
ellisthomas <ellisthomas80@gmail.com>
2018-06-12 21:41:42 -0500

    appC

--- public/javascripts/AppConstant.js
+++
@@ -1,8 +0,0 @@
-app.constant("FIREBASE_CONFIG", {
-	apiKey: "AIzaSyAzd3_tXBIaWltt4SKPFkPGSXrtbobBt-I",
-    authDomain: "portfolio-3d42d.firebaseapp.com",
-    databaseURL: "https://portfolio-3d42d.firebaseio.com",
-    projectId: "portfolio-3d42d",
-    storageBucket: "",
-    messagingSenderId: "155422748462"
-});
